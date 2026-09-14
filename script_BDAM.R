910b7b6309c558db7adb35617fc01a5f57044feb
gabriel-magioli <gabgomes@id.uff.br>
2026-09-14 10:15:59 -0300

    script - tarefa 1 a 2

--- script_BDAM.R
+++ script_BDAM.R
@@ -81,6 +81,10 @@ summary(dados_bd2)
 # Padronizar as categorias SEXO_PROPRIETARIO para Masculino e Feminino
 # Atribuir legendas para a variável TIPO_VEICULO, sendo 1: Carro e 2: Moto
 # Criar uma nova variável em dados_bd2 F_IDADE categorizando as idades em: 22 a 34, 35 a 45
+dados_bd2$SEXO_PROPRIETARIO[dados_bd2$SEXO_PROPRIETARIO %in% c("feminino", "FEMININO")] = "Feminino"
+dados_bd2$SEXO_PROPRIETARIO[dados_bd2$SEXO_PROPRIETARIO %in% c("masculino", "MASCULINO")] = "Masculino"
+dados_bd2$F_IDADE=ifelse(dados_bd2$IDADE_PROPRIETARIO < 35,"22 a 34", "35 a 45")
+dados_bd2$TIPO_VEICULO=factor(dados_bd2$TIPO_VEICULO, levels = c(1,2), labels = c("Carro","Moto"))
 
 # Ao terminar a Tarefa 2 commit com a mensagem " script - tarefa 1 a 2" e envie para o repositório Treino_Extensao
 
